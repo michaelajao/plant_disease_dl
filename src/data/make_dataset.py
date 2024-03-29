85e46b6e00032c6c4bb47a80d26d7ea4ea1e99b5
michaelajao <ajaoolarinoyemichael@gmail.com>
2024-03-29 14:11:42 +0000

    Normalize image data in make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -103,7 +103,7 @@ transform = transforms.Compose(
         transforms.RandomHorizontalFlip(),
         transforms.RandomVerticalFlip(),
         transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
-        # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
 )
 
